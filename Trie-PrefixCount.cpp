ad4cc7ef45021a4d32fd4e4dd939772f5d962198
nicsinha <sinhanikhil097@gmail.com>
2024-01-19 07:38:29 +0530

    Finding count of Prefix in multiple word

---
+++ Trie-PrefixCount.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    Node* links[26];
+    int SC = 0;
+    int EW = 0;
+
+    bool containKey(char ch) {
+        return links[ch-'a']!=NULL;
+    }
+
+    void put(char ch, Node* node){
+        links[ch-'a'] = node;
+    }
+
+    void increaseSC(){
+        SC += 1;
+    }
+    void increaseEW(){
+        EW += 1;
+    }
+    void decreaseSC(){
+        SC -= 1;
+    }
+    void decreaseEW(){
+        EW -= 1;
+    }
+
+    int getSC()
+    {
+        return SC;
+    }
+    int getEW()
+    {
+        return EW;
+    }
+
+    
+
+    Node* next(char ch) {
+        return links[ch-'a'];
+    }
+  
+};
+
+class Trie
+{
+private:
+    Node *root;
+public:
+
+    void insert(string word)
+    {
+        Node* temp = root;
+
+        for (int i = 0; i < word.length(); i++)
+        {
+            if(!temp->containKey(word[i]))
+            {
+                Node *n = new Node();
+                temp->put(word[i],n);
+            }
+            temp = temp->next(word[i]); 
+            temp->increaseSC(); 
+        }
+        temp->increaseEW();
+        temp->increaseSC();
+    }
+
+    int countPrefix(string word)
+    {
+        Node* temp = root;
+
+        for(int i=0;i<word.length();i++)
+        {
+            if(temp->containKey(word[i]))
+            {
+                temp = temp->next(word[i]);
+            }
+            else {
+                return 0;
+            }
+        }
+        return temp->getSC();
+    }
+
+    int countWordEndingWith(string word)
+    {
+        Node* temp = root;
+        for(int i=0;i<word.length();i++)
+        {
+            if(temp->containKey(word[i]))
+            {
+                temp = temp->next(word[i]);
+            }
+            else {
+                return 0;
+            }
+        }
+        return temp->getEW();
+
+    }
+    
+};
+
+
+
