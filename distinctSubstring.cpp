64f62412f5067e2ae65f15fa6d28439e37d3b994
nicsinha <sinhanikhil097@gmail.com>
2024-01-25 22:31:24 +0530

    Finding all unique substring

---
+++ distinctSubstring.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    Node* links[26];
+
+    bool containKey(char ch) {
+        return links[ch - 'a']!=NULL;
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+
+    Node* next(char ch)
+    {
+        return links[ch - 'a'];
+    }
+
+};
+
+class Trie 
+{
+    Node* root;
+
+    int subString = 0;
+
+    public:
+
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+
+        Node *temp = root;
+
+        for(int i=0;i<word.length();i++)
+        {
+            if(!temp->containKey(word[i])) {
+
+                temp->put(word[i],new Node());
+                subString += 1;
+            }
+            temp = temp->next(word[i]);
+            
+        }
+        cout<<word<<"   inserted into Trie"<<endl;
+    }
+    int countString()
+    {
+        return subString;
+    }
+};
+
+int main()
+{
+    string s = "abab";
+
+    Trie trie;
+
+    for(int i=0;i<s.length();i++)
+    {
+        string sub = "";
+        for(int j=i;j<s.length();j++)
+        {
+           sub = sub+s[j];
+        }
+        cout<<sub<<endl;
+        trie.insert(sub);
+    }
+    cout<<trie.countString()+1;
+
+    return 0;
+}
