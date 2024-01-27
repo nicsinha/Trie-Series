62a64513ec38b626b58af2229f1fc0637159cb8a
nicsinha <sinhanikhil097@gmail.com>
2024-01-27 16:37:31 +0530

    Finding maximum xor

---
+++ BitXor.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+
+    Node* links[2];
+
+    bool containkey(int bit)
+    {
+        return links[bit]!=NULL;
+    }
+
+    Node* next(int bit)
+    {
+        return links[bit];
+    }
+
+    void put(int bit,Node* node)
+    {
+        links[bit] = node;
+    }
+};
+
+class Trie {
+
+    Node* root;
+
+    public:
+
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(int num) {
+
+        Node* temp = root;
+
+        for(int i=31;i>=0;i--) {
+
+            int bit = (num>>i) & 1;
+
+            if(!temp->containkey(bit)) {
+
+                temp->put(bit,new Node());
+            }
+            temp = temp->next(bit);
+        }
+    }
+    //Trying to find the oppsite bit of a number, for maximum xor.
+
+    int findMax(int x) {
+
+        Node* temp = root;
+        int ans = 0;
+        for(int i=31;i>=0;i--) {
+            
+            int bit = (x >> i) & 1;
+
+            if(temp->containkey(1 - bit)) {
+
+                ans = ans | (1<<i);
+                temp = temp->next(1- bit);
+            }
+            else {
+                temp = temp->next(bit)
+            }
+        }
+        return ans;
+    }
+};
